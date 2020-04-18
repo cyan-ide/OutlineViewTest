d7d20b332b589c18ee7e05618c47d59adab2a358
adam <adam-westerski@i2r.a-star.edu.sg>
2020-04-18 11:11:03 +0800

    Initial Commit

---
+++ OutlineViewTest/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  OutlineViewTest
+//
+//  Created by adam on 18/4/20.
+//  Copyright © 2020 Adam Westerski. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
